2dc82792cb4eb1ae6e0321afe68d816260b7a7c8
Manjula Dube <dube.manjula668@gmail.com>
2018-03-23 00:19:27 +0530

    reducer for main page

---
+++ redux/home.js
@@ -0,0 +1,25 @@
+export default function(state = {
+    busy: false,
+    repoDetails: null,
+    history: []
+  }, action) {
+      switch(action.type) {
+          case 'SET_BUSY': 
+  
+              return {
+                ...state,
+                busy : action.data.busy
+              }
+  
+          case 'STORE_RESULT': 
+  
+              return {
+                ...state,
+                repoDetails : action.data,
+                history: [...state.history, action.data]
+              }
+          
+          default: 
+              return state
+      }
+  }
