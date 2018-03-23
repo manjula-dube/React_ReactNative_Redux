55aaab86eedc0263129eca637675100f9eb67e96
Manjula Dube <dube.manjula668@gmail.com>
2018-03-23 20:23:47 +0530

    add table structur

--- web/src/components/repositories.js
+++ web/src/components/repositories.js
@@ -2,11 +2,22 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const Repo = props => (
-  <div className='repo'>
-    <a className='name' href={props.url}>
+  <div className='repo-row'>
+    <div className='repo-cell'>
+      <a className='name' href={props.url}>
+        {props.id}
+      </a>
+    </div>
+    <div className='stars repo-cell'>
       {props.name}
-    </a>
-    <div className='stars'>
+    </div>
+    <div className='stars repo-cell'>
+      {props.username}
+    </div>
+    <div className='stars repo-cell'>
+      {props.stars}
+    </div>
+    <div className='stars repo-cell'>
       {props.stars}
     </div>
     <div className='clear' />
@@ -14,11 +25,37 @@ const Repo = props => (
 )
 
 const Repositories = ({ repoDetail }) => (
-  <div className='section repositories'>
+  <div className='section repositories repo-table'>
+    <RepoHeader />
+    {console.log(repoDetail)}
     {repoDetail.repos &&
             repoDetail.repos.map((repo, index) => (
               <Repo key={index} name={repo.name} url={repo.url} stars={repo.stars} />
             ))}
+  </div>
+)
+
+const RepoHeader = props => (
+  <div className='repo-row'>
+    <div className='repo-cell'>
+            ID
+        </div>
+
+    <div className='repo-cell'>
+            Repo Title
+        </div>
+
+    <div className='repo-cell'>
+            Owner
+        </div>
+
+    <div className='repo-cell'>
+            Stars
+        </div>
+    <div className='repo-cell'>
+            Created At
+        </div>
+
   </div>
 )
 
