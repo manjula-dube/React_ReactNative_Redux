cbb2576ea838386a81825295f5798447f85ff141
Manjula Dube <dube.manjula668@gmail.com>
2018-03-23 14:28:24 +0530

    add components

---
+++ web/src/components/user-form.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { get } from "../data-fetcher";
+import { setBusy, storeResult } from "../actions";
+
+class UserForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { disabled: true, userName: "" };
+        this.onSearchUserClick = this.onSearchUserClick.bind(this);
+    }
+
+    onChange(userName) {
+        let disabled = true;
+        if (userName) disabled = false;
+        this.setState({ disabled: disabled });
+        this.setState({ userName });
+    }
+
+    onSearchUserClick() {
+        if (this.props.busy) {
+            return;
+        }
+
+        this.props.dispatch(setBusy(true));
+        get(`https://github-user.now.sh?username=${this.state.userName}`).then(data => {
+            this.props.dispatch(setBusy(false));
+            this.props.dispatch(storeResult(data.data));
+        });
+    }
+
+    render() {
+        return (
+            <div className="user-form">
+                <input
+                    onChange={event => this.onChange(event.target.value)}
+                    type="text"
+                    placeholder="github username"
+                    value={this.state.userName}
+                />
+                <button
+                    className={this.props.busy ? "busy" : ""}
+                    onClick={this.onSearchUserClick}
+                    disabled={this.state.disabled}
+                >
+                    See profile
+                </button>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        busy: state.home.busy,
+    };
+}
+
+export default connect(mapStateToProps)(UserForm);
---
+++ web/src/components/user-info.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const UserInfo = ({ repoDetail }) => (
+    <div className="section user-info">
+        <img className="avatar" src={repoDetail.avatar} />
+        <a className="name" href={repoDetail.url}>
+            {repoDetail.name}
+        </a>
+        <div className="description">
+            {repoDetail.description}
+        </div>
+    </div>
+);
+
+function mapStateToProps(state) {
+    return {
+        repoDetail: state.home.repoDetails || {},
+    };
+}
+
+export default connect(mapStateToProps)(UserInfo);
