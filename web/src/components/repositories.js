5e6d4065765dfc4b9105b5e29daba8fb8f43cfae
manjula91 <dube.manjula668@gmail.com>
2018-03-24 12:06:33 +0530

    Change repository to class

--- web/src/components/repositories.js
+++ web/src/components/repositories.js
@@ -1,69 +1,53 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react"
+import { connect } from "react-redux"
 
 const Repo = props => (
-  <div className='repo-row'>
-    <div className='repo-cell'>
-      <a className='name' href={props.url}>
-        {props.id}
-      </a>
-    </div>
-    <div className='stars repo-cell'>
-      {props.name}
-    </div>
-    <div className='stars repo-cell'>
-      {props.username}
-    </div>
-    <div className='stars repo-cell'>
-      {props.stars}
-    </div>
-    <div className='stars repo-cell'>
-      {props.stars}
-    </div>
-    <div className='clear' />
-  </div>
+	<div className="repo-row">
+		<div className="repo-cell">
+			<a className="name" href={props.url}>
+				{props.id}
+			</a>
+		</div>
+		<div className="stars repo-cell">{props.name}</div>
+		<div className="stars repo-cell">{props.username}</div>
+		<div className="stars repo-cell">{props.stars}</div>
+		<div className="stars repo-cell">{props.stars}</div>
+		<div className="clear" />
+	</div>
 )
 
-const Repositories = ({ repoDetail }) => (
-  <div className='section repositories repo-table'>
-    <RepoHeader />
-    {console.log(repoDetail)}
-    {repoDetail.repos &&
-            repoDetail.repos.map((repo, index) => (
-              <Repo key={index} name={repo.name} url={repo.url} stars={repo.stars} />
-            ))}
-  </div>
-)
+class Repositories extends Component {
+	render() {
+		const { repoDetail } = this.props
+		return (
+			<div className="section repositories repo-table">
+				<RepoHeader />
+				{repoDetail.repos &&
+					repoDetail.repos.map((repo, index) => (
+						<Repo key={index} name={repo.name} url={repo.url} stars={repo.stars} />
+					))}
+			</div>
+		)
+	}
+}
 
 const RepoHeader = props => (
-  <div className='repo-row'>
-    <div className='repo-cell'>
-            ID
-        </div>
+	<div className="repo-row">
+		<div className="repo-cell">ID</div>
 
-    <div className='repo-cell'>
-            Repo Title
-        </div>
+		<div className="repo-cell">Repo Title</div>
 
-    <div className='repo-cell'>
-            Owner
-        </div>
+		<div className="repo-cell">Owner</div>
 
-    <div className='repo-cell'>
-            Stars
-        </div>
-    <div className='repo-cell'>
-            Created At
-        </div>
-
-  </div>
+		<div className="repo-cell">Stars</div>
+		<div className="repo-cell">Created At</div>
+	</div>
 )
 
-function mapStateToProps (state) {
-  console.log(state.home.repoDetails)
-  return {
-    repoDetail: state.home.repoDetails || {}
-  }
+function mapStateToProps(state) {
+	return {
+		repoDetail: state.home.repoDetails || {},
+	}
 }
 
 export default connect(mapStateToProps)(Repositories)
