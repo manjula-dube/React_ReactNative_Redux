5653f3831df9f00efdb39f09e091f1b2707562e0
Manjula Dube <dube.manjula668@gmail.com>
2018-03-24 18:55:32 +0530

    add logo

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Header = () => (
   <div className='header'>
-    <img src='logo.png' className='logo' alt='default' />
+    <img src='../../logo.png' className='logo' alt='default' />
   </div>
 )
 
