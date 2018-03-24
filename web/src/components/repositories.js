ba797a1505de351d11d694eeee33bdcf3191dfc6
manjula91 <dube.manjula668@gmail.com>
2018-03-24 13:24:08 +0530

    Paginate response

--- web/src/components/repositories.js
+++ web/src/components/repositories.js
@@ -9,23 +9,102 @@ const Repo = props => (
 			</a>
 		</div>
 		<div className="stars repo-cell">{props.name}</div>
-		<div className="stars repo-cell">{props.username}</div>
-		<div className="stars repo-cell">{props.stars}</div>
-		<div className="stars repo-cell">{props.stars}</div>
+		<div className="stars repo-cell">{props.owner}</div>
+		<div className="stars repo-cell">{props.stargazers_count}</div>
+		<div className="stars repo-cell">{new Date(props.created_at).toDateString()}</div>
 		<div className="clear" />
 	</div>
 )
 
 class Repositories extends Component {
-	render() {
+	constructor() {
+		super()
+
+		this.PAGE_SIZE = 5
+
+		this.state = {
+			currentPage: 1,
+		}
+
+		this.onPageClick = this.onPageClick.bind(this)
+	}
+
+	onPageClick(event) {
+		event.persist()
+		const currentPage = event.currentTarget.getAttribute("data-index")
+		this.setState({
+			currentPage,
+		})
+	}
+
+	calculateMinMax() {
+		const { currentPage } = this.state
+		const { PAGE_SIZE } = this
+
+		const minIndex = (currentPage - 1) * PAGE_SIZE,
+			maxIndex = minIndex + PAGE_SIZE
+
+		return { minIndex, maxIndex }
+	}
+
+	renderTable() {
 		const { repoDetail } = this.props
+
+		const { minIndex, maxIndex } = this.calculateMinMax()
+
+		if (repoDetail.repos) {
+			return repoDetail.repos
+				.slice(minIndex, maxIndex)
+				.map((repo, index) => (
+					<Repo
+						key={repo.id}
+						id={repo.id}
+						name={repo.name}
+						stargazers_count={repo.stargazers_count}
+						url={repo.url}
+						created_at={repo.created_at}
+						stars={repo.stars}
+						owner={repo.owner.login}
+					/>
+				))
+		} else {
+			return null
+		}
+	}
+
+	renderControlBar() {
+		const maxPage = this.getMaxPages()
+
+		return (
+			<div>
+				{Array(maxPage)
+					.join("|")
+					.split("|")
+					.map((item, index) => {
+						return (
+							<span data-index={index + 1} onClick={this.onPageClick}>
+								{index + 1}{" "}
+							</span>
+						)
+					})}
+			</div>
+		)
+	}
+
+	getMaxPages() {
+		if (this.props.repoDetail.repos && this.props.repoDetail.repos.length) {
+			return parseInt(this.props.repoDetail.repos.length / this.PAGE_SIZE) + 1
+		} else {
+			return 0
+		}
+	}
+
+	render() {
 		return (
 			<div className="section repositories repo-table">
 				<RepoHeader />
-				{repoDetail.repos &&
-					repoDetail.repos.map((repo, index) => (
-						<Repo key={index} name={repo.name} url={repo.url} stars={repo.stars} />
-					))}
+				{this.renderTable()}
+				{this.renderControlBar()}
 			</div>
 		)
 	}
