b4cc948809022e15be4be6b77f8611362db4c28c
manjula91 <dube.manjula668@gmail.com>
2018-03-24 11:57:25 +0530

    Request network only when data is not in store

--- redux-store/store/home.js
+++ redux-store/store/home.js
@@ -1,27 +1,31 @@
 const home = (
-    state = {
-      busy: false,
-      repoDetails: null,
-      history: []
-    },
-    action
+	state = {
+		busy: false,
+		repoDetails: null,
+		history: [],
+	},
+	action
 ) => {
-  console.log('action :  ', action)
-  switch (action.type) {
-    case 'SET_BUSY':
-      return Object.assign({}, state, {
-        busy: action.data.busy
-      })
+	switch (action.type) {
+		case "SET_BUSY":
+			return Object.assign({}, state, {
+				busy: action.data.busy,
+			})
 
-    case 'STORE_RESULT':
-      return Object.assign({}, state, {
-        repoDetails: action.data,
-        history: [...state.history, action.data]
-      })
+		case "RENDER_RESULT":
+			return Object.assign({}, state, {
+				repoDetails: action.data,
+			})
 
-    default:
-      return state
-  }
+		case "STORE_RESULT":
+			return Object.assign({}, state, {
+				repoDetails: action.data,
+				history: [...state.history, action.data],
+			})
+
+		default:
+			return state
+	}
 }
 
 export default home
